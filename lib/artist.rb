ca24850256b62acfab6468af9e1c1ed62fa9b972
cissica <cecemartinez@hotmail.com>
2020-10-31 13:26:00 -0700

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,31 @@
+class Artist
+    attr_accessor :name
+  
+    @@all_songs = 1
+
+    def initialize(name)
+      @name = name
+      @songs = []
+    end
+  
+    def add_song(song)
+      self.songs << song
+      song.artist = self
+      @@all_songs +=1
+    end
+  
+    def add_song_by_name(name)
+      song = Song.new(name)
+      self.songs << song
+      song.artist = self
+      @@all_songs +=1
+    end
+  
+    def songs
+        Song.all.select {|song| song.artist == self}
+    end
+  
+    def self.song_count
+      @@all_songs
+    end
+  end
